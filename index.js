bfa16b6d17e4489fe45a501b59d0d03d1511ecf4
Slonetech <kengeregordon@gmail.com>
2023-07-12 11:47:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const arrayOfLetters = word.split('');
+  const reverseLetters = arrayOfLetters.reverse()
+  const reverseString  = reverseLetters.join('')
+
+  if (word == reverseString){
+
+    return true
+
+  } else {
+    return false
+  }
 }
 
 /* 
-  Add your pseudocode here
+  Function isPalindrome has to take a string as a parameter. If the string is palindrome, the function should return true.
+   If not then the function should return false
 */
-
 /*
-  Add written explanation of your solution here
+  The function takes the word and splits each letter into an array, then the array is reversed.
+   The array is then joined again into a word. Then we compare if the original word is the same as the reversed word.
+    If true then it returns true, or else it will return false
 */
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log('Expecting: false')
+  console.log('=>', isPalindrome('slow'));
+
+    //✓ returns false for 'ab'
+
+  console.log('Expecting: true')
+  console.log('=>', isPalindrome('abba'));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
